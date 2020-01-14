67391e0c0e3bbec4b5dfcf46638773091242916d
shuang1330 <shuangli1330@gmail.com>
2020-01-14 11:24:33 +0100

    changed file path

--- predict.sh
+++ predict.sh
@@ -5,7 +5,7 @@ model_path=$2
 prediction_savepath=$3
 log_path=$4
 
-python /groups/umcg-gcc/tmp04/umcg-sli/variant_prioritization/model_inference_tool/commands_scripts/model_inference.py \
+python CAPICE_scripts/model_inference.py \
 --input_path $cadd_annotated_path \
 --model_path $model_path \
 --prediction_savepath $prediction_savepath \
