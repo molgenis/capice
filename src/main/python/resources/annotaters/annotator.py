c6447e96953087d8e3a1fcce15bec09e6d466df5
SietsmaRJ <robertjarik@hotmail.com>
2021-06-16 12:06:19 +0200

    WIP CAPICE 3.0
    Changelog:
    
    - convert_vep_vcf_to_tsv_capice.sh: added support for the "HGNC_ID" gene identifier and "SYMBOL_SOURCE" for the source of the gene identifier.
    - annotator.py: added support for the SEQ column processed in CADD.
    - lookup.py: implemented pysam FastaFile to process SEQ requests in annotator.py

--- src/main/python/resources/annotaters/annotator.py
+++ src/main/python/resources/annotaters/annotator.py
@@ -1,29 +1,39 @@
 from src.main.python.core.logger import Logger
 from src.main.python.core.global_manager import CapiceManager
 from src.main.python.resources.annotaters.processer import ProcessorAnnotator
-from src.main.python.resources.annotaters.lookup import LookupAnnotator
+from src.main.python.resources.annotaters.lookup import LookupAnnotator, FastaLookupAnnotator
 import pandas as pd
 
 
 class Annotator:
-    def __init__(self):
+    def __init__(self, dataset: pd.DataFrame):
         self.log = Logger().logger
         self.manager = CapiceManager()
+        self.fasta_lookup = FastaLookupAnnotator()
+        self.dataset = dataset
 
-    def annotate(self, dataset: pd.DataFrame):
+    def annotate(self):
         """
         Start the annotation process.
-        :param dataset: pandas dataframe
         :return: pandas dataframe similar to the output of the CADD pipeline
         """
         self.log.info('Starting annotation processing')
-        annotated_dataset = dataset  # change once everything is ready
+        annotated_dataset = self.dataset  # change once everything is ready
         return annotated_dataset
 
-    @staticmethod
-    def _correct_percentage_sign(dataset: pd.DataFrame):
+    def _correct_percentage_sign(self):
         new_columns = []
-        for column in dataset.columns:
+        for column in self.dataset.columns:
             new_columns.append(column.split('%')[1])
-        dataset.columns = new_columns
-        return dataset
+        self.dataset.columns = new_columns
+
+    def _add_sequence(self, dataset: pd.DataFrame):
+        self.log.debug('Annotation addition: sequence')
+        self.dataset['SEQ'] = dataset.apply(
+            lambda x: self.fasta_lookup.get_reference_sequence(
+                chromosome=x['CHROM'],
+                start=x['POS'] - 75,
+                end=x['POS'] + 75
+            ), axis=1
+        )
+        self.fasta_lookup.close_connection()
--- src/main/python/resources/annotaters/lookup.py
+++ src/main/python/resources/annotaters/lookup.py
@@ -1,2 +1,55 @@
+import pysam
+from src.main.python.core.global_manager import CapiceManager
+from src.main.python.core.logger import Logger
+
+
+class FastaLookupAnnotator:
+    def __init__(self):
+        self.log = Logger().logger
+        self.manager = CapiceManager()
+        self.fasta_loc = self.manager.reference_genome
+        self.fasta = None
+        self._load_fasta()
+
+    def _load_fasta(self):
+        self.log.info('Loading in Fasta file, this may take a moment.')
+        self.fasta = pysam.FastaFile(self.fasta_loc)
+        self.log.info('Succesfully loaded Fasta file at: {}'.format(self.fasta_loc))
+
+    def get_reference_sequence(self, chromosome: str, start: int, end: int):
+        """
+        Function to obtain a sequence from the reference Fasta file.
+
+        :param chromosome: string, chromosome to get the reference sequence from.
+        :param start: Chromosomal position at what point the sequence should be obtained.
+        :param end: Chromosomal position at what point the obtained sequence should end.
+        :return: string, obtained reference sequence.
+        """
+        try:
+            self.log.debug(
+                'Obtaining reference sequence for: [Chromosome: {}], [start: {}], [stop: {}]'.format(chromosome, start,
+                                                                                                     end))
+            append_ns = False
+            if start < 0:
+                append_ns = abs(start)
+                start = 0
+            return_sequence = self.fasta.fetch(chromosome, start, end)
+            if append_ns:
+                return_sequence = '{}{}'.format('N' * append_ns, return_sequence)
+            return return_sequence
+        except KeyError:
+            self.log.warning(
+                'Unable to obtain sequence for: [Chromosome: {}], [start: {}], [stop: {}],'
+                'did you supply a reference with contigs 1-22 + x,y,mt?'.format(chromosome, start, end))
+            return None
+
+    def close_connection(self):
+        """
+        Function to tell pysam to close the connection to the Fasta file
+        """
+        if self.fasta:
+            self.fasta.close()
+
+
 class LookupAnnotator:
     pass
