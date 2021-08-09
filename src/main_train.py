5f7968b78ae3f6a37d3c036965a863a89b839c9c
SietsmaRJ <robertjarik@hotmail.com>
2021-08-09 14:58:40 +0200

    Updated both main_train.py and the test for main train

--- src/main_train.py
+++ src/main_train.py
@@ -11,7 +11,8 @@ from sklearn.model_selection import train_test_split, RandomizedSearchCV
 
 class Train(Main):
     """
-    Train class of CAPICE to create new CAPICE like models for new or specific use cases.
+    Train class of CAPICE to create new CAPICE like models for new or specific
+    use cases.
     """
 
     def __init__(self,
@@ -28,26 +29,41 @@ class Train(Main):
 
         # Argument logging
         self.balance = self.config.get_train_value('makebalanced')
-        self.log.debug('Make input dataset balanced confirmed: {}'.format(self.balance))
+        self.log.debug(
+            'Make input dataset balanced confirmed: {}'.format(self.balance)
+        )
         self.default = self.config.get_train_value('default')
-        self.log.debug('The use of the default Python 3.6 hyperparameters set to: {}'.format(self.default))
-        self.specified_default = self.config.get_train_value('specifieddefaults')
-        self.log.debug('The use of specified default hyperparameters set to: {}'.format(self.specified_default))
+        self.log.debug(
+            'The use of the default Python 3.6 hyperparameters set to: '
+            '{}'.format(self.default)
+        )
+        self.specified_default = self.config.get_train_value(
+            'specifieddefaults'
+        )
+        self.log.debug(
+            'The use of specified default hyperparameters set to: '
+            '{}'.format(self.specified_default)
+        )
         self.n_split = self.config.get_train_value('split')
-        self.log.debug('Split has been confirmed, set to: {}'.format(self.n_split))
+        self.log.debug(
+            'Split has been confirmed, set to: {}'.format(self.n_split)
+        )
         self.early_exit = self.config.get_train_value('earlyexit')
         if self.early_exit:
             self.log.debug('Early exit flag confirmed.')
         self.train_test_size = self.config.get_train_value('traintestsize')
-        self.log.debug('The percentage of data used for the testing dataset within training: {}'.format(
-            self.train_test_size))
+        self.log.debug(
+            'The percentage of data used for the '
+            'testing dataset within training: {}'.format(
+                self.train_test_size)
+        )
 
         # Global variables
         self.random_state = 45
         self.split_random_state = 4
         self.model_random_state = 0
         self.defaults = {}
-        self.cadd_features = []
+        self.annotation_features = []
         self.processed_features = []
         self.verbose = self.manager.verbose
         self.model_type = None
@@ -56,7 +72,8 @@ class Train(Main):
 
     def run(self):
         """
-        Main function. Will make a variety of calls to the required modules in order to create new CAPICE models.
+        Main function. Will make a variety of calls to the required modules in
+        order to create new CAPICE models.
         """
         data = self._rename_chrom_col(self.load_file())
         train_checker = TrainChecker()
@@ -64,23 +81,36 @@ class Train(Main):
         if self.balance:
             data = self.process_balance_in_the_force(dataset=data)
         if self.n_split > 0.0:
-            self.log.info('Splitting input dataset before any preprocessing happens.')
-            data, _ = self.split_data(dataset=data, test_size=self.n_split, export=True)
+            self.log.info(
+                'Splitting input dataset before any preprocessing happens.'
+            )
+            data, _ = self.split_data(
+                dataset=data, test_size=self.n_split,
+                export=True
+            )
         if self.balance:
-            self.exporter.export_capice_training_dataset(datafile=data,
-                                                         feature='balanced dataset',
-                                                         name='balanced_dataset')
+            self.exporter.export_capice_training_dataset(
+                datafile=data,
+                feature='balanced dataset',
+                name='balanced_dataset'
+            )
         self.load_defaults()
         if self.early_exit:
             exit('Early exit command was called, exiting.')
-        imputed_data = self.impute(loaded_cadd_data=data)
-        self.cadd_features = self.manager.cadd_features
-        processed_data = self.preprocess(loaded_cadd_data=imputed_data, train=True)[1]
+        imputed_data = self.impute(loaded_data=data)
+        self.annotation_features = self.manager.annotation_features
+        processed_data = self.preprocess(
+            loaded_data=imputed_data, train=True
+        )[1]
         self._get_processed_features(dataset=processed_data)
-        processed_train, processed_test = self.split_data(dataset=processed_data,
-                                                          test_size=self.train_test_size)
+        processed_train, processed_test = self.split_data(
+            dataset=processed_data,
+            test_size=self.train_test_size
+        )
         model = self.train(test_set=processed_test, train_set=processed_train)
-        self.exporter.export_capice_model(model=model, model_type=self.model_type)
+        self.exporter.export_capice_model(
+            model=model, model_type=self.model_type
+        )
 
     @staticmethod
     def _rename_chrom_col(dataset):
@@ -89,35 +119,50 @@ class Train(Main):
 
     def split_data(self, dataset, test_size: float, export: bool = False):
         """
-        Function to split any given dataset into 2 datasets using the test_size argument. Can export both if export
-        flag is enabled.
+        Function to split any given dataset into 2 datasets using the test_size
+        argument. Can export both if export flag is enabled.
         :param dataset: pandas.DataFrame
         :param test_size: float, ranging 0-1
         :param export: boolean
         :return: train, test (1-test_size, test_size)
         """
-        train, test = train_test_split(dataset, test_size=test_size, random_state=self.split_random_state)
+        train, test = train_test_split(
+            dataset,
+            test_size=test_size,
+            random_state=self.split_random_state
+        )
         if export:
-            self.exporter.export_capice_training_dataset(datafile=train,
-                                                         name='splitted_train_dataset',
-                                                         feature='splitted train')
-            self.exporter.export_capice_training_dataset(datafile=test,
-                                                         name='splitted_test_dataset',
-                                                         feature='splitted test')
+            self.exporter.export_capice_training_dataset(
+                datafile=train,
+                name='splitted_train_dataset',
+                feature='splitted train'
+            )
+            self.exporter.export_capice_training_dataset(
+                datafile=test,
+                name='splitted_test_dataset',
+                feature='splitted test'
+            )
         return train, test
 
     def load_defaults(self):
         """
-        Function to load in specified default hyper parameters. If no specified default is supplied, but -d flag is
-        used, load in the original hyper parameters. Note: in any case the original hyper parameters will be loaded,
-        but due to the absence of the -d or -sd flag, the self.defaults will not be used.
+        Function to load in specified default hyper parameters.
+        If no specified default is supplied, but -d flag is used,
+        load in the original hyper parameters.
+        Note: in any case the original hyper parameters will be loaded,
+        but due to the absence of the -d or -sd flag,
+        the self.defaults will not be used.
         """
         if self.specified_default:
             train_checker = TrainChecker()
             with open(self.specified_default) as json_file:
                 defaults = json.load(json_file)
             train_checker.check_specified_defaults(loaded_defaults=defaults)
-            self.log.debug('Specified defaults located at {} successfully loaded.'.format(self.specified_default))
+            self.log.debug(
+                'Specified defaults located at {} successfully loaded.'.format(
+                    self.specified_default
+                )
+            )
             self.default = True
         else:
             defaults = {
@@ -129,7 +174,8 @@ class Train(Main):
 
     def process_balance_in_the_force(self, dataset: pd.DataFrame):
         """
-        Balancing function as first used by Li et al. in the original CAPICE paper. Balances baced on Consequence,
+        Balancing function as first used by Li et al. in the original
+        CAPICE paper. Balances baced on Consequence,
         allele frequency and benign/pathogenic samples.
         :param dataset: pandas.DataFrame
         :return: balanced pandas.DataFrame
@@ -140,11 +186,13 @@ class Train(Main):
         anakin = pd.DataFrame(columns=dataset.columns)
         bins = [0, 0.01, 0.05, 0.1, 0.5, 1]
         for consequence in palpatine['Consequence'].unique():
-            processed_consequence = self._process_consequence(pathogenic_dataframe=palpatine,
-                                                              benign_dataframe=yoda,
-                                                              return_df_columns=anakin.columns,
-                                                              bins=bins,
-                                                              consequence=consequence)
+            processed_consequence = self._process_consequence(
+                pathogenic_dataframe=palpatine,
+                benign_dataframe=yoda,
+                return_df_columns=anakin.columns,
+                bins=bins,
+                consequence=consequence
+            )
             anakin = anakin.append(processed_consequence)
         self.log.info('Balancing complete.')
         return anakin
@@ -158,7 +206,9 @@ class Train(Main):
         self.log.debug("Processsing: {}".format(consequence))
         selected_pathogenic = pathogenic_dataframe[
             pathogenic_dataframe['Consequence'] == consequence]
-        selected_neutral = benign_dataframe[benign_dataframe['Consequence'] == consequence]
+        selected_neutral = benign_dataframe[
+            benign_dataframe['Consequence'] == consequence
+            ]
         if selected_pathogenic.shape[0] > selected_neutral.shape[0]:
             selected_pathogenic = selected_pathogenic.sample(
                 selected_neutral.shape[0],
@@ -173,11 +223,13 @@ class Train(Main):
             upper_bound = bins[ind]
             lower_bound = bins[ind + 1]
             sample_number = selected_pathogenic_histogram[ind]
-            processed_bins = self._process_bins(selected_pathogenic=selected_pathogenic,
-                                                selected_neutral=selected_neutral,
-                                                upper_bound=upper_bound,
-                                                lower_bound=lower_bound,
-                                                sample_num=sample_number)
+            processed_bins = self._process_bins(
+                selected_pathogenic=selected_pathogenic,
+                selected_neutral=selected_neutral,
+                upper_bound=upper_bound,
+                lower_bound=lower_bound,
+                sample_num=sample_number
+            )
             return_df = return_df.append(processed_bins)
         return return_df
 
@@ -210,13 +262,15 @@ class Train(Main):
                 random_state=self.random_state
             )
         self.log.debug(
-            "Sampled {} variants from Possibly Neutral Variants in range of: {} - {}".format(
+            "Sampled {} variants from Possibly Neutral Variants in range of: "
+            "{} - {}".format(
                 selected_pnv_currange.shape[0],
                 lower_bound,
                 upper_bound)
         )
         self.log.debug(
-            "Sampled {} variants from Possibly Pathogenic Variants in range of: {} - {}".format(
+            "Sampled {} variants from Possibly Pathogenic Variants in range "
+            "of: {} - {}".format(
                 selected_pathogenic_currange.shape[0],
                 lower_bound,
                 upper_bound)
@@ -226,7 +280,8 @@ class Train(Main):
     @staticmethod
     def _get_vars_in_range(variants: pd.DataFrame, upper: float, lower: float):
         """
-        Sub-function of the balancing function to get variants within a certain allele frequency threshold.
+        Sub-function of the balancing function to get variants within a certain
+        allele frequency threshold.
         :param variants: pandas.DataFrame
         :param upper: float
         :param lower: float
@@ -240,22 +295,25 @@ class Train(Main):
 
     def _get_processed_features(self, dataset: pd.DataFrame):
         """
-        Function to save the columns of a dataset that have been processed and thus are an output column of the CADD
-        annotation.
+        Function to save the columns of a dataset that have been processed and
+        thus are an output column of the CADD annotation.
         :param dataset: pandas.DataFrame
         """
         for column in dataset.columns:
-            for feature in self.cadd_features:
+            for feature in self.annotation_features:
                 if column == feature or column.startswith(feature):
                     if column not in self.processed_features:
                         self.processed_features.append(column)
 
     def train(self, test_set: pd.DataFrame, train_set: pd.DataFrame):
         """
-        The training part of main_train.py after all has been processed. This is the same as Li et al. originally used
-        to create CAPICE, but might be altered due to deprecation of certain libraries.
-        :param test_set: pandas.DataFrame, the testing dataset for determine performance during training
-        :param train_set: pandas.DataFrame, the training dataset on which the model will be created on
+        The training part of main_train.py after all has been processed.
+        This is the same as Li et al. originally used to create CAPICE,
+        but might be altered due to deprecation of certain libraries.
+        :param test_set: pandas.DataFrame,
+            the testing dataset for determine performance during training
+        :param train_set: pandas.DataFrame,
+            the training dataset on which the model will be created on
         """
         param_dist = {
             'max_depth': scipy.stats.randint(1, 20),
@@ -304,18 +362,20 @@ class Train(Main):
             ransearch1 = model_estimator
             self.model_type = 'XGBClassifier'
         else:
-            model_estimator = xgb.XGBClassifier(verbosity=verbosity,
-                                                objective='binary:logistic',
-                                                booster='gbtree', n_jobs=n_jobs,
-                                                min_child_weight=1,
-                                                max_delta_step=0,
-                                                subsample=1, colsample_bytree=1,
-                                                colsample_bylevel=1,
-                                                colsample_bynode=1,
-                                                reg_alpha=0, reg_lambda=1,
-                                                scale_pos_weight=1,
-                                                base_score=0.5,
-                                                random_state=self.model_random_state)
+            model_estimator = xgb.XGBClassifier(
+                verbosity=verbosity,
+                objective='binary:logistic',
+                booster='gbtree', n_jobs=n_jobs,
+                min_child_weight=1,
+                max_delta_step=0,
+                subsample=1, colsample_bytree=1,
+                colsample_bylevel=1,
+                colsample_bynode=1,
+                reg_alpha=0, reg_lambda=1,
+                scale_pos_weight=1,
+                base_score=0.5,
+                random_state=self.model_random_state
+            )
             ransearch1 = RandomizedSearchCV(estimator=model_estimator,
                                             param_distributions=param_dist,
                                             scoring='roc_auc', n_jobs=8,
