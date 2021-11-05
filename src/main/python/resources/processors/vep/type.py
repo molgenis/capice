71d8ed5e064ae23ac6d80581a54073caef10e09e
mkslofstra <mariska-karina@hotmail.com>
2021-11-05 13:08:34 +0100

    fix(type): fix missing DELINS type

--- src/main/python/resources/processors/vep/type.py
+++ src/main/python/resources/processors/vep/type.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from src.main.python.resources.enums.sections import Column
 from src.main.python.resources.processors.vep.template import Template
 
@@ -11,21 +12,33 @@ class Type(Template):
         )
 
     def process(self, dataframe: pd.DataFrame):
+        """
+        process variants to annotate their types
+        :param dataframe: a dataframe with as columns at least a ref and an alt
+        :return: a dataframe with an added types column
+
+        if len(ref) == 1 && len (alt) == 1:
+            type = 'SNV'
+        elif ref[0] == alt &&  len(alt) == 1:
+            type = 'DEL'
+        elif alt[0] == ref && len(ref) == 1:
+            type = 'INS'
+        else:
+            type = 'DELINS'
+        """
+        dataframe['Type'] = 'DELINS'
         dataframe.loc[
             dataframe[
-                dataframe[Column.ref.value].str.len() == dataframe[
-                    Column.alt.value].str.len()].index,
-            'Type'] = 'SNV'
+                (dataframe[Column.ref.value].str.len() == 1) & (
+                            dataframe[Column.alt.value].str.len() == 1)].index, 'Type'] = 'SNV'
         dataframe.loc[
             dataframe[
-                dataframe[Column.ref.value].str.len() > dataframe[
-                    Column.alt.value].str.len()].index,
-            'Type'] = 'DEL'
+                (dataframe[Column.ref.value].str.get(0) == dataframe[Column.alt.value]) & (
+                        dataframe[Column.alt.value].str.len() == 1)].index, 'Type'] = 'DEL'
         dataframe.loc[
             dataframe[
-                dataframe[Column.ref.value].str.len() < dataframe[
-                    Column.alt.value].str.len()].index,
-            'Type'] = 'INS'
+                (dataframe[Column.alt.value].str.get(0) == dataframe[Column.ref.value]) & (
+                        dataframe[Column.ref.value].str.len() == 1)].index, 'Type'] = 'INS'
         return dataframe
 
     @property
