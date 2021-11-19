f3fe5fe96938669ebd40b706781f0ff46ded20d6
SietsmaRJ <robertjarik@hotmail.com>
2021-11-19 12:49:04 +0100

    Point 20: Split validators.py (part 2)
    - Moved tests for post_file_parse_validator.py and post_vep_processing_validator.py to each and their own separate testing module.
    - Moved test_input_validator.py to its righteous validator folder.

--- src/test/python/validators/test_post_file_parse_validator.py
+++ src/test/python/validators/test_post_file_parse_validator.py
@@ -1,12 +1,8 @@
-import os
-import pickle
 import unittest
 import pandas as pd
 
 from src.test.python.test_templates import teardown
-from src.main.python.validators.post_vep_processing_validator import PostVEPProcessingValidator
 from src.main.python.validators.post_file_parse_validator import PostFileParseValidator
-from src.main.python.utilities.utilities import get_project_root_dir
 
 
 class TestPostFileParseValidator(unittest.TestCase):
@@ -81,44 +77,6 @@ class TestPostFileParseValidator(unittest.TestCase):
             ValueError,
             self.validator.validate_chrom_pos,
             incorrect_dataset
-        )
-
-
-class TestPostVEPProcessingValidator(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        print('Setting up.')
-        cls.dataset = pd.DataFrame(
-            {
-                'chr': [1, 2],
-                'pos': [100, 200],
-                'ref': ['A', 'A'],
-                'alt': ['T', 'T'],
-                'feat1': ['foo', 'bar']
-            }
-        )
-        with open(
-                os.path.join(
-                    get_project_root_dir(),
-                    'CAPICE_model',
-                    'GRCh37',
-                    'POC',
-                    'xgb_booster_poc.pickle.dat'
-                ), 'rb'
-        ) as model_file:
-            cls.validator = PostVEPProcessingValidator(pickle.load(model_file))
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        print('Tearing down.')
-        teardown()
-
-    def test_validate_features_present_incorrect(self):
-        print('KeyError raise due to missing VEP processed feature')
-        self.assertRaises(
-            KeyError,
-            self.validator.validate_features_present,
-            self.dataset
         )
 
 
