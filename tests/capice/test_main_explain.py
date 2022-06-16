0bd204d3d41e465f9ae392ba1fea36ddde075711
SietsmaRJ <work.robertsietsma@gmail.com>
2022-06-16 16:00:49 +0200

    feat: CAPICE explain
    - Started writing test file for the entirety of CAPICE explain. WIP.

---
+++ tests/capice/test_main_explain.py
@@ -0,0 +1,18 @@
+import unittest
+
+import os
+
+import pandas as pd
+
+from tests.capice.test_templates import _project_root_directory
+from molgenis.capice.main_explain import CapiceExplain
+
+
+class TestCapiceExplain(unittest.TestCase):
+    def test_capice_explain(self):
+        model = None
+        output_path = None
+
+
+if __name__ == '__main__':
+    unittest.main()
