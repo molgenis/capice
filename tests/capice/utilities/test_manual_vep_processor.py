599bb9bc4766687a6b677b7123f0218d9db8171b
Sander van den Hoek <svandenhoek@users.noreply.github.com>
2022-11-17 09:16:16 +0100

    Revert "feat: Improved intron-exon splitting"

--- tests/capice/utilities/test_manual_vep_processor.py
+++ tests/capice/utilities/test_manual_vep_processor.py
@@ -89,13 +89,7 @@ class TestAnnotator(unittest.TestCase):
                 'is_splice_donor_region_variant': {0: 0, 1: 0},
                 'is_splice_polypyrimidine_tract_variant': {0: 0, 1: 0},
                 'Type': {0: 'SNV', 1: 'SNV'},
-                'Length': {0: 0, 1: 0},
-                'Exon_number': {0: 5, 1: np.nan},
-                'Exon_number_affected': {0: 1, 1: np.nan},
-                'Exon_total': {0: 5, 1: np.nan},
-                'Intron_number': {0: np.nan, 1: np.nan},
-                'Intron_number_affected': {0: np.nan, 1: np.nan},
-                'Intron_total': {0: np.nan, 1: np.nan},
+                'Length': {0: 0, 1: 0}
             }
         )
         expected_outcome = pd.concat(
@@ -110,7 +104,9 @@ class TestAnnotator(unittest.TestCase):
                         'SourceID',
                         'HGNC_ID',
                         'FeatureID',
-                        'STRAND'
+                        'STRAND',
+                        'Exon',
+                        'Intron'
                     ]
                 ],
                 expected_processed_columns
