169978f6d5d8568b3bbe5dde081aacbab658d15d
Sander van den Hoek <savandenhoek@gmail.com>
2022-12-12 13:02:57 +0100

    Minor inline-doc update.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
             'pytest',  # pytest
             'coverage',  # coverage run -m pytest --junitxml=results.xml && coverage html
             'mypy',  # mypy --ignore-missing-imports src/
-            'flake8',  # flake8 src/
+            'flake8',  # flake8 src/ tests/
             'flake8-import-order'
         ]
     },
