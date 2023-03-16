3320e166bb49e7fbbc14bfbf025fb45b29bc6665
Sander van den Hoek <savandenhoek@gmail.com>
2023-03-16 13:38:56 +0100

    Uses most severe consequence only.

--- src/molgenis/capice/vep/consequence.py
+++ src/molgenis/capice/vep/consequence.py
@@ -19,71 +19,16 @@ class Consequence(Template):
 
     @property
     def columns(self):
-        return ['is_regulatory_region_variant',
-                'is_regulatory_region_ablation',
-                'is_regulatory_region_amplification',
-                'is_missense_variant',
-                'is_intron_variant',
-                'is_upstream_gene_variant',
-                'is_downstream_gene_variant',
-                'is_synonymous_variant',
-                'is_TF_binding_site_variant',
-                'is_splice_donor_variant',
-                'is_coding_sequence_variant',
-                'is_splice_region_variant',
-                'is_stop_gained',
-                'is_splice_acceptor_variant',
-                'is_frameshift_variant',
-                'is_3_prime_UTR_variant',
-                'is_inframe_insertion',
-                'is_inframe_deletion',
-                'is_5_prime_UTR_variant',
-                'is_start_lost',
-                'is_non_coding_transcript_exon_variant',
-                'is_non_coding_transcript_variant',
-                'is_TFBS_ablation',
-                'is_TFBS_amplification',
-                'is_protein_altering_variant',
-                'is_stop_lost',
-                'is_stop_retained_variant',
-                'is_transcript_ablation',
-                'is_intergenic_variant',
-                'is_start_retained_variant',
-                'is_transcript_amplification',
-                'is_incomplete_terminal_codon_variant',
-                'is_mature_miRNA_variant',
-                'is_NMD_transcript_variant',
-                'is_feature_elongation',
-                'is_feature_truncation',
-                'is_splice_donor_5th_base_variant',
-                'is_splice_donor_region_variant',
-                'is_splice_polypyrimidine_tract_variant'
-                ]
+        return ['most_severe_consequence']
 
     @property
     def dtypes(self) -> list[str]:
-        return ['boolean']*39
+        return ['category']
 
     @staticmethod
     def _fillna():
         return 0
 
     def _process(self, dataframe: pd.DataFrame):
-        splitted_consequence = dataframe[self.name].str.split('&', expand=True)
-        raw_consequences = []
-        for consequence in self.columns:
-            current_consequence = consequence.split('is_')[1]
-            dataframe[consequence] = np.where(
-                np.isin(splitted_consequence, current_consequence).any(axis=1), 1, 0
-            )
-            raw_consequences.append(current_consequence)
-
-        self._validate_consequences(splitted_consequence, raw_consequences)
+        dataframe[self.columns[0]] = dataframe[self.name].str.split('&').str[0]
         return dataframe
-
-    def _validate_consequences(self, consequences: pd.DataFrame, supported_consequences: list):
-        unique_consequences = pd.Series(pd.unique(consequences.values.ravel('K'))).dropna()
-        for consequence in unique_consequences:
-            if consequence not in supported_consequences:
-                self.log.warning('Supplied VEP consequence: %s is not supported in the '
-                                 'Consequence processor!', consequence)
--- tests/capice/vep/test_consequence.py
+++ tests/capice/vep/test_consequence.py
@@ -7,71 +7,23 @@ from molgenis.capice.vep.consequence import Consequence
 
 
 class TestConsequence(unittest.TestCase):
-    def setUp(self) -> None:
-        self.data = pd.DataFrame(
-            {
-                'Consequence': ['transcript_ablation&stop_lost', 'start_lost', np.nan]
-            }
-        )
-        self.expected_data = pd.DataFrame(
-                    {
-                        'is_regulatory_region_variant': {0: 0, 1: 0, 2: 0},
-                        'is_regulatory_region_ablation': {0: 0, 1: 0, 2: 0},
-                        'is_regulatory_region_amplification': {0: 0, 1: 0, 2: 0},
-                        'is_missense_variant': {0: 0, 1: 0, 2: 0},
-                        'is_intron_variant': {0: 0, 1: 0, 2: 0},
-                        'is_upstream_gene_variant': {0: 0, 1: 0, 2: 0},
-                        'is_downstream_gene_variant': {0: 0, 1: 0, 2: 0},
-                        'is_synonymous_variant': {0: 0, 1: 0, 2: 0},
-                        'is_TF_binding_site_variant': {0: 0, 1: 0, 2: 0},
-                        'is_splice_donor_variant': {0: 0, 1: 0, 2: 0},
-                        'is_coding_sequence_variant': {0: 0, 1: 0, 2: 0},
-                        'is_splice_region_variant': {0: 0, 1: 0, 2: 0},
-                        'is_stop_gained': {0: 0, 1: 0, 2: 0},
-                        'is_splice_acceptor_variant': {0: 0, 1: 0, 2: 0},
-                        'is_frameshift_variant': {0: 0, 1: 0, 2: 0},
-                        'is_3_prime_UTR_variant': {0: 0, 1: 0, 2: 0},
-                        'is_inframe_insertion': {0: 0, 1: 0, 2: 0},
-                        'is_inframe_deletion': {0: 0, 1: 0, 2: 0},
-                        'is_5_prime_UTR_variant': {0: 0, 1: 0, 2: 0},
-                        'is_start_lost': {0: 0, 1: 1, 2: 0},
-                        'is_non_coding_transcript_exon_variant': {0: 0, 1: 0, 2: 0},
-                        'is_non_coding_transcript_variant': {0: 0, 1: 0, 2: 0},
-                        'is_TFBS_ablation': {0: 0, 1: 0, 2: 0},
-                        'is_TFBS_amplification': {0: 0, 1: 0, 2: 0},
-                        'is_protein_altering_variant': {0: 0, 1: 0, 2: 0},
-                        'is_stop_lost': {0: 1, 1: 0, 2: 0},
-                        'is_stop_retained_variant': {0: 0, 1: 0, 2: 0},
-                        'is_transcript_ablation': {0: 1, 1: 0, 2: 0},
-                        'is_intergenic_variant': {0: 0, 1: 0, 2: 0},
-                        'is_start_retained_variant': {0: 0, 1: 0, 2: 0},
-                        'is_transcript_amplification': {0: 0, 1: 0, 2: 0},
-                        'is_incomplete_terminal_codon_variant': {0: 0, 1: 0, 2: 0},
-                        'is_mature_miRNA_variant': {0: 0, 1: 0, 2: 0},
-                        'is_NMD_transcript_variant': {0: 0, 1: 0, 2: 0},
-                        'is_feature_elongation': {0: 0, 1: 0, 2: 0},
-                        'is_feature_truncation': {0: 0, 1: 0, 2: 0},
-                        'is_splice_donor_5th_base_variant': {0: 0, 1: 0, 2: 0},
-                        'is_splice_donor_region_variant': {0: 0, 1: 0, 2: 0},
-                        'is_splice_polypyrimidine_tract_variant': {0: 0, 1: 0, 2: 0}
-                    }
-                )
-        self.expected_data = self.expected_data.astype('boolean')
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.consequence = Consequence()
 
-    def test_consequence(self):
-        data_copy = self.data.copy(deep=True)
-        observerd = Consequence().process(self.data)
-        # if numpy.array dtype not given,
-        # then the type will be determined as the minimum type required to hold the
-        # objects in the sequence. this minimal type is system dependent.
-        expected = pd.concat(
-            [
-                data_copy,
-                self.expected_data
-            ], axis=1
-        )
-        pd.testing.assert_frame_equal(observerd.sort_index(axis=1), expected.sort_index(
-            axis=1), check_dtype=False)
+    def test_process(self):
+        dataframe = pd.DataFrame({
+            'Consequence': ['transcript_ablation&stop_lost', 'start_lost', np.nan]
+        })
+        observed = self.consequence.process(dataframe)
+        expected = pd.DataFrame({
+            'Consequence': ['transcript_ablation&stop_lost', 'start_lost', np.nan],
+            'most_severe_consequence': ['transcript_ablation', 'start_lost', np.nan],
+        })
+        expected = expected.astype({
+            'Consequence': 'object', 'most_severe_consequence': 'category'
+        })
+        pd.testing.assert_frame_equal(expected, observed)
 
     def test_non_coding(self):
         data = pd.DataFrame({
