ebf1efd72ab33e8af12dd416c86d6d21ff682afe
Sander van den Hoek <savandenhoek@gmail.com>
2023-03-27 13:09:13 +0200

    Removed not-used code.

--- src/molgenis/capice/utilities/enums.py
+++ src/molgenis/capice/utilities/enums.py
@@ -63,31 +63,6 @@ class OutputClasses(Enum):
     damaging = ''
 
 
-class TrainEnums(Enum):
-    """
-    Enums to use within the training module of CAPICE.
-    """
-    Consequence = 'Consequence'
-    max_AF = 'max_AF'
-    sample_weight = 'sample_weight'
-    binarized_label = 'binarized_label'
-    learning_rate = 'learning_rate'
-    max_depth = 'max_depth'
-    n_estimators = 'n_estimators'
-
-
-class Domains(Enum):
-    """
-    Enums specific to the VEP processor that does the processing for Domains.
-    """
-    ncoils = 0
-    signalp = 1
-    lowcomplex = 2
-    ndomain = 3
-    panther = 4
-    other = 5
-
-
 class UniqueSeparator(Enum):
     """
     Enum specific to creating a specific separator for the preservation of the chr pos ref alt
