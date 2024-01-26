2caee95b7578b82cba5c2e49b353b368b8c87c39
bart.charbon <b.charbon@umcg.nl>
2024-01-26 11:48:11 +0100

    Remove Gnomad_HN

--- tests/capice/test_main_train.py
+++ tests/capice/test_main_train.py
@@ -106,8 +106,7 @@ class TestMainTrain(unittest.TestCase):
             'is_NMD_transcript_variant', 'is_feature_elongation', 'is_feature_truncation',
             'SpliceAI_pred_DP_AG', 'SpliceAI_pred_DP_AL', 'SpliceAI_pred_DP_DG',
             'SpliceAI_pred_DP_DL', 'SpliceAI_pred_DS_AG', 'SpliceAI_pred_DS_AL',
-            'SpliceAI_pred_DS_DG', 'SpliceAI_pred_DS_DL', 'Type', 'Length', 'Grantham', 'phyloP',
-            'gnomAD_HN'
+            'SpliceAI_pred_DS_DG', 'SpliceAI_pred_DS_DL', 'Type', 'Length', 'Grantham', 'phyloP'
         ]
         self.assertSetEqual(set(observed), set(expected))
 
